be28a9c8a3ec57db793a9389c12e831d8c21615d
Berk Erdemoglu <berkerdemoglu1120@gmail.com>
2021-04-11 13:40:50 +0300

    move triangle to shapes
    since a lot of shapes will be used in the 3d engine, moved triangle to a separate file named shapes.py. in a future commit, a base class called Shape should be created.

---
+++ A3E/shapes.py
@@ -0,0 +1,7 @@
+class Triangle():
+    """A class for a triangle in the engine"""
+
+    def __init__(self,vertex1,vertex2,vertex3):
+        self.vertex1 = vertex1
+        self.vertex2 = vertex2
+        self.vertex3 = vertex3
