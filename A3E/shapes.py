6c631bdef861408d196986d1fdfba097da458e54
Berk Erdemoglu <berkerdemoglu1120@gmail.com>
2021-04-11 13:54:26 +0300

    add base class, shape (not used)
    created a base class for all shapes in the 3d engine, it is currently not in use.

--- A3E/shapes.py
+++ A3E/shapes.py
@@ -1,7 +1,12 @@
+class Shape():
+    """TODO: Write a shape class which will be the parent class for all shapes in the engine."""
+    pass
+
+
 class Triangle():
     """A class for a triangle in the engine"""
 
-    def __init__(self,vertex1,vertex2,vertex3):
+    def __init__(self, vertex1, vertex2, vertex3):
         self.vertex1 = vertex1
         self.vertex2 = vertex2
         self.vertex3 = vertex3
