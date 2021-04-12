047beed146d41ee1b25b3a7a5fc1d264917736fe
Emre Cenk <emrecenk9@gmail.com>
2021-04-12 17:49:02 -0400

    wrote quadrilateral class

--- graphics/shapes_2d.py
+++ graphics/shapes_2d.py
@@ -1,5 +1,5 @@
 import pygame
-from Mathematical_Functions.projecting import project_3d_point_to_2d, project_triangle
+from Mathematical_Functions.projecting import project_triangle
 class shape_2d:
 
     def __init__(self,color,fill_bool=False):
@@ -11,6 +11,8 @@ class shape_2d:
         for tri in self.triangles:
             height = window.get_height()
             width = window.get_width()
+
+            #getting the 2d points:
             v1_2d,v2_2d,v3_2d = project_triangle(tri.vertex1,tri.vertex2,tri.vertex3,width,height)
 
             if self.fill:
@@ -26,7 +28,7 @@ class shape_2d:
 
 
 class triangle(shape_2d):
-    def __init__(self,vertex1,vertex2,vertex3,color = "white",fill_bool=True):
+    def __init__(self,vertex1,vertex2,vertex3,color = "white",fill_bool=False):
         super().__init__(color,fill_bool)
         self.vertex1 = vertex1
         self.vertex2 = vertex2
@@ -37,9 +39,22 @@ class triangle(shape_2d):
 
 
 
-# class quadrilateral:
-#     def __init__(self,vertex1,vertex2,vertex3,vertex4):
-#
-#
-#     def convert_to_triangles(self,top_left,bottom_right):
+class quadrilateral(shape_2d):
+    def __init__(self,v1,v2,v3,v4,_color = "white", fill_bool = False):
+
+        super().__init__(color=_color)
+        self.convert_to_triangles(v1,v2,v3,v4)
+
+    def convert_to_triangles(self,v1,v2,v3,v4):
+        self.triangles.append(
+            triangle(
+            v1,v2,v3,color = self.color, fill_bool=self.fill
+        )
+        )
+
+        self.triangles.append(
+            triangle(
+                v3[0], v4[1], v1[2], color=self.color, fill_bool=self.fill
+            )
+        )
 
