0586c0bf4ec346805f171f9f9d11beb4550c481c
Emre Cenk <emrecenk9@gmail.com>
2021-04-29 19:00:50 -0400

    fixed fill_top function. Tested using the actual built in triangle drawing system and it works perfectly

--- graphics/managing_window_pixels.py
+++ graphics/managing_window_pixels.py
@@ -4,7 +4,7 @@ from pygame import gfxdraw
 
 from typing import Sequence
 
-class window_manager:
+class WindowManager:
     #This class implements a z buffer
 
     def __init__(self, width:int = None, height: int = None) :
@@ -50,54 +50,6 @@ class window_manager:
             gfxdraw.pixel(window, index[0], index[1], self.changed_pixels[index])
 
 
-    def draw_line(self, window, x1, y1, x2, y2, color = (255,255,255)):
-
-        # An implementation of Bresenham's Line algorithm
-        # The algorithm basically travels through all integer x coordinates between the two points and finds the
-        # corresponding y value by keeping track of the y difference between points
-
-        mn = 2 * (y2 - y1)
-        sn = mn - (x2 - x1)
-
-        y = y1
-        for x in range(int(x1), int(x2) + 1):
-            self.draw_proper_pixel(window, x, y , 0)
-            sn += mn
-            if sn >= 0:
-                y += 1
-                sn -= 2 * (x2 - x1)
-    def fill_top(self,window, v1, v2, v3):
-        inverse_slope_1 = (v3[0] - v1[0]) / (v3[1] - v1[1])
-        inverse_slope_2 = (v3[0] - v2[0]) / (v3[1] - v2[1])
-        curx1 = v3[0]
-        curx2 = v3[0]
-
-        s_line_y = v3[1]
-        while s_line_y > v1[1]:
-
-            self.draw_line(window, curx1, s_line_y, curx2, s_line_y)
-
-            curx1 -= inverse_slope_1
-            curx2 -= inverse_slope_2
-            s_line_y -= 1
-
-
-    def fill_bottom(self,window, v1,v2,v3):
-
-        invslope1 = (v2[0] - v1[0]) / (v2[1] - v1[1])
-        invslope2 = (v3[0] - v1[0]) / (v3[1] - v1[1])
-        curx1 = curx2 = v3[0]
-
-        s_line_y = v1
-        while s_line_y <= v2[1]:
-            self.draw_line(window, curx1, s_line_y, curx2, s_line_y)
-            curx1 += invslope1
-            curx2 += invslope2
-
-            s_line_y += 1
-
-
-
 
     @staticmethod
     def sort_v_by_ascending(arr: Sequence) -> None:
@@ -113,43 +65,60 @@ class window_manager:
             if (arr[1] < arr[0]):
                 arr[1], arr[0] = arr[0], arr[1]
 
+        return arr
 
-    def draw_triangle(self, v1: Sequence,
-                            v2: Sequence,
-                            v3: Sequence,
+
+    def flat_fill_top(self, surface, v1: Sequence, v2: Sequence, v3: Sequence, color:Sequence = (255,255,255)):
+        
+        """
+        v1: bottom right corner of triangle
+        v2: bottom left corner
+        v3: top of triangle
+
+        Assumes that the y values of v1 and v2 are the same. 
+        This function uses the fact that increasing y by one increases x by the inverse slope of the line:
+        y=mx+b
+        (y-b)/m = x
+        (y+1-b)/m = x + (1/m)
+        Using this fact, we loop through all the y values given, and find the respective x values"""
+
+        inverse_m2 = (v2[0]-v3[0]) / (v2[1] - v3[1])
+        inverse_m1 = (v1[0]-v3[0]) / (v1[1] - v3[1])
+
+        current_x_1 = v1[0]
+        current_x_2 = v2[0]
+        current_y = v1[1]
+
+        while current_y>=v3[1]:
+            pygame.draw.line(surface,color, (current_x_1, current_y), (current_x_2, current_y))
+            current_y -= 1
+            current_x_1 -= inverse_m1
+            current_x_2 -= inverse_m2
+
+        
+        
+ 
+    def draw_triangle(self, v1: Sequence,v2: Sequence,v3: Sequence,
                             v1_distance: float, v2_distance: float, v3_distance: float):
+
 
         """ This function rasterizes the given triangle and draws each pixel onto the screen.
         The pixel is not drawn if there is anything else in that pixel which is closer.
         The algorithm basically splits the triangle into 2 triangles where each triangle has one side parallel to the x axis."""
 
 
-
-
+        
 
 
 if __name__ == '__main__':
     k=0
     from time import perf_counter
-    screen = window_manager(500,500)
+    screen = WindowManager(500,500)
     pygame.init()
 
     window = pygame.display.set_mode((500,500), pygame.RESIZABLE)
 
-    #PERFORMANCE TEST:
     
-    pixels = pygame.PixelArray(window)
-    a=10
-    total=0
-    for k in range(a):
-        s = perf_counter()
-
-        for i in range(500):
-            for j in range(500):
-                pixels[i][j] = 1000
-
-        total+=perf_counter()-s
-                # gfxdraw.pixel(window,i,j,(255,255,255))
 
     #gfxdraw.pixel takes around 0.2 seconds for 250,000 pixels
     #pygame.surfarray.pixels3d takes 0.3 seconds
@@ -157,10 +126,19 @@ if __name__ == '__main__':
     #pygame.draw.circle takes 0.3 seconds
 
     #with cython pixelarray takes 0.02 seconds
-    screen.render_all_pixels()
-    print('on average:',total/a)
-    print(k)
+
+
     while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                break
+
+        screen.flat_fill_top(window, [300,100],[100,100], [19,19])
+        screen.render_all_pixels()
         pygame.display.update()
 
+        pygame.time.delay(100)
+        pygame.draw.polygon(window,(255,0,0),points = [[300,100],[100,100], [19,19]] )
 
